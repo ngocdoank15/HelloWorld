01b141952a08c259b50e7b77774a7b2bf81a4218
vinh.duc.nguyen <vinh.duc.nguyen@logigear.com>
2016-04-27 14:02:47 +0700

    s

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,4 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+	
+	public void getMessage() {
+        return "Hello World";
+    }
 }
