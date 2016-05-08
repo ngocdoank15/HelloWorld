5345b1e82c7fb44909a972c32b3f40a1b3ee6d87
ngocdoank15 <hongngock15tpm@gmail.com>
2016-05-08 10:55:10 +0700

    s

--- src/HelloWorldTest.java
+++ src/HelloWorldTest.java
@@ -3,15 +3,12 @@ public class HelloWorldTest extends junit.framework.TestCase {
     public void testNothing() {
     }
     
-    public void testWillAlwaysPass() {
+    public void testWillAlwaysFail() {
 		oata.HelloWorld h = new oata.HelloWorld();
 		assertEquals(h.getMessage(),"Hello World");
         //fail("An error message");
     }
 
-   public void testWillAlwaysFail() {
-		
-        fail("An error message");
-    }
+  
     
 }
