dc4dfaa9955f75f1fe493ae653cf2bd44f063440
vinh.duc.nguyen <vinh.duc.nguyen@logigear.com>
2016-05-31 15:06:41 +0700

    sss

---
+++ src/oata/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package oata;
+public class HelloWorldTest extends junit.framework.TestCase {
+
+    public void testNothing() {
+    }
+    
+    public void testWillAlwaysFail() {
+		//oata.HelloWorld h = new oata.HelloWorld();
+		//assertEquals(h.getMessage(),"Hello World");
+        fail("An error message");
+    }
+
+  
+    
+}
