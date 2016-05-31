1e2c39a496f4670cc260dfb3a90ffc01ed0fb121
vinh.duc.nguyen <vinh.duc.nguyen@logigear.com>
2016-05-31 15:22:03 +0700

    s

--- src/oata/MergeSortTest.java
+++ src/oata/MergeSortTest.java
@@ -1,22 +1,26 @@
 package oata;
+import java.util.Arrays;
+
+import oata.*;
 
 public class MergeSortTest extends junit.framework.TestCase {
-	String input = "3,2,7,4,1,5,4,2,3";
-	//long staTime = System.nanoTime();
-	//JSONObject json = new JSONObject();
-	String[] inputArr = input.split(",");
-	MergeSort mms = new MergeSort();
-	int[] intArr = mms.toIntArr(inputArr);
+	 public void testWillAlwaysFail() {
+	    	String input = "3,2,7,4,1,5,4,2,3";
+	    	//long staTime = System.nanoTime();
+	    	//JSONObject json = new JSONObject();
+	    	String[] inputArr = input.split(",");
+	    	MergeSort mms = new MergeSort();
+	    	int[] intArr = mms.toIntArr(inputArr);
 
-	// print(intArr);
+	    	
+	    	mms.array = intArr;
+	    	mms.length = inputArr.length;
+	    	mms.tempMergArr = new int[mms.length];
+	    	//mms.doMergeSort(0, intArr.length-1);
+	    	mms.mergeParts(0, 4, 8);
+	    	// print(intArr);
+	    	assertTrue(Arrays.equals(intArr, mms.array ));
+	    	System.out.println("Xong");
+	    }
 
-	mms.array = intArr;
-	mms.length = inputArr.length;
-	mms.tempMergArr = new int[mms.length];
-	//mms.doMergeSort(0, intArr.length-1);
-	mms.mergeParts(0, 4, 8);
-	// print(intArr);
-	Assert.assertArrayEquals( inputArr, mms.array );
-
-	System.out.println("Xong");
 }
